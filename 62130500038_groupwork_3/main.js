d2a6e967e548698325b815f01ecab7a72016dc45
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-02-11 23:28:19 +0700

    [UPDATE] All Element
    Delete tailwind template and Finish all element

--- 62130500038_groupwork_3/main.js
+++ 62130500038_groupwork_3/main.js
@@ -5,34 +5,40 @@ const app = {
         {
           url: "images/pineapple.jpg",
           title: "Pineapple",
-          description:"This is the picture of Pineapple",
-          liked:false
+          description: "This is the picture of Pineapple",
+          liked: false
         },
         {
           url: "images/lion.jpg",
           title: "Lion",
-          description:"This is the picture of Lion",
-          liked:false
+          description: "This is the picture of Lion",
+          liked: false
         },
         {
           url: "images/surface.jpg",
           title: "Microsoft Surface",
-          description:"This is the picture of Microsoft Surface",
-          liked:false
+          description: "This is the picture of Microsoft Surface",
+          liked: false
         },
+        {
+          url: "images/bangkok-landscape.jpg",
+          title: "Bangkok",
+          description: "This is the picture of Bangkok",
+          liked: false
+        }
       ],
     };
   },
   methods: {
-      toggleLike(index) {
-          this.photos[index].liked = !this.photos[index].liked
-        //   console.log(this.photos[index].liked)
-      }
+    toggleLike(index) {
+      this.photos[index].liked = !this.photos[index].liked
+      //   console.log(this.photos[index].liked)
+    }
   },
-  computed:{
-      countLike(){
-        return this.photos.filter(t => t.liked).length
-      }
+  computed: {
+    countLike() {
+      return this.photos.filter(t => t.liked).length
+    }
   }
 };
 Vue.createApp(app).mount("#app");
